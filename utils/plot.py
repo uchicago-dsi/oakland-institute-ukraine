58ca70e786a47c9e366ff33c1a6307aeb2100f63
Josemaría Macedo <jmacedoc@Josemarias-MacBook-Pro.local>
2023-07-19 11:00:31 -0700

    Minor changes to utils modules to run docker image correctly

--- utils/plot.py
+++ utils/plot.py
@@ -1,7 +1,7 @@
 # Name: Josemaria Macedo Carrillo
 # Title: Plot functions
 # Created: 07/14/23
-# Last modified: -
+# Last modified: 07/19/23
 # DSI
 
 import matplotlib.pyplot as plt
@@ -67,6 +67,7 @@ def plot_line(x_axis, y_axis, line_labels, graph_title, x_label, y_label):
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.legend()
+
     plt.savefig("output/" + standard_name(graph_title) + ".png")
 
 def plot_crops(crop, df_1, df_2):
