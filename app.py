ff41eddf0cb3311af2f13800a61b09ff373cd9f3
Josemaría Macedo <jmacedoc@Josemarias-MacBook-Pro.local>
2023-07-20 09:48:25 -0700

    Minor changes to add other plots. When running docker image the figure outputs are not in output directory still

--- app.py
+++ app.py
@@ -1,46 +1,33 @@
 # Name: Josemaria Macedo Carrillo
 # Title: App file
 # Created: 07/17/23
-# Last modified: 07/19/23
+# Last modified: 07/20/23
 # DSI
 
 import os
 import pandas as pd
-from utils.clean_data import rename_columns, create_columns, translate_column
-from utils.record_linkage import find_matches
+from utils.get_data import get_data
+from utils.clean_data import PRODUCTS_VAL
 from utils.plot import cargo_grouping, plot_line, plot_crops
 
 pd.set_option('display.float_format', lambda x: '%.4f' % x)
 
-# Import data
-# ADD THIS LATER AS FUNCTION IN READ_DATA MODULE
-current_dir = os.path.dirname(os.path.abspath(__file__))
-ig_path = os.path.join(current_dir, "data/import_genius/kernel_10000.csv")
-bsgi_path = os.path.join(current_dir, "data/bsgi_outbound_shipments.csv")
-
-ig = pd.read_csv(ig_path, parse_dates=["EXPORT DATE"])
-bsgi = pd.read_csv(bsgi_path, thousands=",", parse_dates=["Departure date"])
-
-## Rename columns so they have standardized column name format (lowercase, no
-# spaces and no special characters)
-ig_c = ig.copy()
-bsgi_c = bsgi.copy()
-ig_c = rename_columns(ig_c)
-bsgi_c = rename_columns(bsgi_c)
-bsgi_c = bsgi_c.rename(columns={"metric_tons": "weight_ton"})
-
-# Create columns with specific formats
-create_columns(ig_c, "ig")
-create_columns(bsgi_c, "bsgi")
+# Import clean data
+ig = get_data("ig_kernel_10000.csv", "ig")
+bsgi = get_data("bsgi_outbound_shipments.csv", "bsgi")
 
 # Create graph image outputs and save them in "output" directory
 # Graph 1. Total volume of exports from Import Genius and BSGI
-ig_g = cargo_grouping(ig_c, ["year", "month"], ["weight_ton"], ["year", "month"], True)
-bsgi_g = cargo_grouping(bsgi_c, ["year", "month"], ["weight_ton"], ["year", "month"], True)
+ig_g = cargo_grouping(ig, ["year", "month"], ["weight_ton"], ["year", "month"], True)
+bsgi_g = cargo_grouping(bsgi, ["year", "month"], ["weight_ton"], ["year", "month"], True)
 ig_g["date"] = ig_g["month"].astype(str) + "/" + ig_g["year"].astype(str)
 bsgi_g["date"] = bsgi_g["month"].astype(str) + "/" + bsgi_g["year"].astype(str)
 
 graph_1 = ig_g.merge(bsgi_g, on="date", suffixes=("_ig", "_bsgi"))
-plot_line(graph_1["date"], [graph_1["weight_ton_ig"], graph_1["weight_ton_bsgi"]], ["Kernel", "Black Sea Grain Initiative"], "BSGI and Kernel volume of exports", "Export date (m-yy)", "Products exported (tons)")
+plot_line(graph_1["date"], [graph_1["weight_ton_ig"], graph_1["weight_ton_bsgi"]], ["Kernel", "Black Sea Grain Initiative"], "BSGI and Kernel volume of exports", "Export date (m-yy)", "Products exported (tons)", True)
 
-# Graph 2. Total volume of exports from Import Genius
+# Graph 2. Total volume of exports from Import Genius and BSGI per crop
+product_std = set(PRODUCTS_VAL)
+
+for crop in product_std:
+    plot_crops(crop, ig, bsgi, True)
