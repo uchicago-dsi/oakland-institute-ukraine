4089244d466192bebf1a174c82c96f811ba2fa3c
Josemaría Macedo <jmacedoc@Josemarias-MacBook-Pro.local>
2023-07-19 17:36:51 -0700

    Created get_data module to have general get_data function that imports and cleans data for further analysis

---
+++ utils/get_data.py
@@ -0,0 +1,63 @@
+# Name: Josemaria Macedo Carrillo
+# Title: Read data
+# Created: 07/18/23
+# Last modified: 07/19/23
+# DSI
+
+import os
+import pandas as pd
+from .clean_data import rename_columns, create_columns, translate_column, clean_column
+
+def import_data(file_name, source):
+    """
+    Import data from .csv file name
+
+    Inputs:
+        file_name (str): name of the .csv file
+        source (str): data source, either "ig" (Import Genius) or "bsgi" (Black
+            Sea Grain Initiative).
+
+    Return (DataFrame): dataframe.
+    """
+
+    current_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    
+    if source == "ig":
+        path = os.path.join(current_dir, "data/import_genius", file_name)
+        df = pd.read_csv(path, parse_dates=["EXPORT DATE"])
+    
+    else:
+        path = os.path.join(current_dir, "data/", file_name)
+        df = pd.read_csv(path, thousands=",", parse_dates=["Departure date"])
+    
+    return df
+
+def get_data(file_name, source):
+    """
+    Get clean data from .csv file name
+
+    Inputs:
+        file_name (str): name of the .csv file
+        source (str): data source, either "ig" (Import Genius) or "bsgi" (Black
+            Sea Grain Initiative).
+
+    Return (DataFrame): dataframe with cleaned data (with renamed columns and
+        new columns neccesary for analysis).
+    """
+    assert source == "ig" or source == "bsgi", "Wrong data source Error: source\
+                                                must be either 'ig' or 'bsgi'."
+    
+    df = import_data(file_name, source)
+    df = rename_columns(df, source)
+    create_columns(df, source)
+    
+    columns = ["country", "product"]
+    for col in columns:
+        clean_column(df, col)
+
+    if source == "bsgi":
+        translate_column(df, "product_std", "google", "en", "uk")
+
+    return df
+
+    
