35cc12fcde39dd2ca7bd96dab5743a8fab8f9e9f
Josemaría Macedo <jmacedoc@Josemarias-MacBook-Pro.local>
2023-09-04 12:22:21 -0500

    Changes to estimate_weights function to allow estimating shares for different companies and countries

--- utils/transform_data.py
+++ utils/transform_data.py
@@ -5,9 +5,10 @@
 # DSI
 
 from .record_linkage import filter_crop, test_crop
+from .plot import cargo_grouping, plot_pie
 import pandas as pd
 
-def estimate_weights(company_df, company_col, bsgi_df, bsgi_col, crop=None):
+def estimate_weights(company_df, company_col, company_add, bsgi_df, bsgi_col, plot_title, crop=None):
     """
     Estimate the export weights for a specific crop.
 
@@ -15,24 +16,35 @@ def estimate_weights(company_df, company_col, bsgi_df, bsgi_col, crop=None):
         company_df (DataFrame): company dataframe with data from Import Genius
             or Panjiva
         company_col (str): column name from the company dataframe that we want
+            to group by
+        company_add (str): column name from the company dataframe that we want
             to add
         bsgi_df (DataFrame): BSGI dataframe
-        bsgi_col (str): column name from the BSGI dataframe that we want to add.
-        crop (str): crop we want to use to estimate export weights
+        bsgi_col (str): column name from the BSGI dataframe that we want to add
+        plot_title (str): title for plot
+        crop (str): crop we want to use to estimate export weights.
 
-    Returns (DataFrame): dataframe with total export weight for a specific crop
-    and company in one row and export weight for a specific crop in the BSGI
-    dataframe in the other row.
+    Returns (Plot): pie plot with export shares by company.
     """
-    test_crop(crop)
-    
-    # CHANGE THIS SO IT CAN ALLOW MORE THAN ONE COMPANY BY GETTING WEIGHT BY
-    # GROUPING BY COMPANY WITH CARGO_GROUPING FUNCTION MAYBE
     
     if crop is None:
-        row_1 = company_df[company_col].sum()
-        row_2 = bsgi_df[bsgi_col].sum()
+        company_g = cargo_grouping(company_df, [company_col], [company_add], [company_col], True, {company_add: "sum"})
+        bsgi_weight = bsgi_df[bsgi_col].sum()
+        print("bsgi_weight: ", bsgi_weight)
+        company_weight = company_g["weight_ton"].sum()
+        print("ig_weight: ", company_weight)
+        other = bsgi_weight - company_weight
+
+        bsgi_dict = {company_col: 'Other', bsgi_col: [other]}
+        bsgi_final = pd.DataFrame(bsgi_dict)
+        final = pd.concat([company_g, bsgi_final], ignore_index=True)
+        final = final.sort_values(by=[bsgi_col], ascending=False)
+        final
+
+        return plot_pie(final[company_col], final[bsgi_col], company_col, plot_title, "Company data for October 2022", 2, 0.1)
+    
     else:
+        test_crop(crop)
         try:
             row_1 = filter_crop(company_df, crop, "ig")[company_col].sum()
         except:
