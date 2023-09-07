7a2dd3cd6ad07a463d86bca9bfb1120da30fd66f
Josemaría Macedo <jmacedoc@Josemarias-MacBook-Pro.local>
2023-09-07 00:34:15 -0500

    Added get_company_dict, standard_company_name and create_wide_table functions to be able to create a wide table for input of the plot_stack_bar function from the plot module

--- utils/transform_data.py
+++ utils/transform_data.py
@@ -58,3 +58,83 @@ def estimate_weights(company_df, company_col, company_add, bsgi_df, bsgi_col, pl
     df = pd.DataFrame(data=d)
     
     return df
+
+def get_company_dict(company_col, company_dict):
+    """
+    Get a dictionary with raw company names (shipper) as keys and standardized
+    company names as values.
+
+    Inputs:
+        company_col (Series): series with raw company names in
+            Ukrainian
+        company_dict (dict): dictionary with parent companies in English as keys
+            and subsiary companies in Ukrainian as values.
+    
+    Return (dict): dictionary with raw company names (shipper) as keys and
+        standardized company names as values.
+    """
+    all_companies = company_col.unique()
+    rename_dict = {}
+
+    for row in all_companies:
+        for parent, subsidiaries in company_dict.items():
+            for subsidiary in subsidiaries:
+                if subsidiary in row:
+                    rename_dict[row] = parent
+        if row not in rename_dict.keys():
+            rename_dict[row] = "Other"
+    
+    return rename_dict
+
+def standard_company_name(company_col, company_dict):
+    """
+    Create new column with standardized company names from "column" parameter.
+
+    Inputs:
+        company_col (Series): series with raw company names in
+            Ukrainian
+        company_dict (dict): dictionary with parent companies in English as keys
+            and subsiary companies in Ukrainian as values.
+    
+    Return (Series): series with new standardized company names.
+    """
+    rename_dict = get_company_dict(company_col, company_dict)
+    standard_col = company_col.apply(lambda x: rename_dict[x])
+    
+    return standard_col
+
+def create_wide_table(df, group, other_cols, sort, asc_bool, agg_dict,
+                   new_name = None):
+    """
+    Create wide table from dataframe.
+
+    Inputs:
+        df (DataFrame): dataset to be used
+        group (lst): list of columns to group by
+        other_cols(lst): list of columns that are going to be aggregated
+        sort (lst): list of columns to sort dataframe by. If new_name is not
+            empty then the new column names should be used
+        asc_bool (bool): boolean stating wheter or not to sort data "ascending"
+            (True) or "descending" (False)
+        agg_dict (dict): dict with aggregation functions we want to apply to
+            data. If we want to aggregate a column by more than one function
+            we put the functions as a list. Example: {"var": ["count", "sum"]}.
+        new_name (lst): optional paramater with list of new names for grouped
+            dataframe. Default is "None".
+
+    Return(DataFrame): wide table with month-year and standardized company names
+    as columns
+    """
+    # First we get long table by grouping data
+    df_g = cargo_grouping(df, group, other_cols, sort, asc_bool, agg_dict)
+    df_g["date"] = df_g["month"].astype(str) + "/" + df_g["year"].astype(str)
+    
+    # Now we get the wide table format
+    pivot = pd.pivot_table(data=df_g, index=['date', "year", "month"],
+                           columns=['company_std'],
+                           values='weight_ton').reset_index()
+    pivot = pivot.sort_values(by=["year", "month"]).drop(['year', 'month'],
+                                                         axis=1).round(1)
+    
+    return pivot
+    
