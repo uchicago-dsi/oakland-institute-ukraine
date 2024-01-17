0176abe64dc00c08f1a8293b635218faafd9b8dd
Josemaria Macedo <josemaria@uchicago.edu>
2024-01-17 17:28:35 -0600

    File to run data pipeline

---
+++ pipeline.py
@@ -0,0 +1,30 @@
+# Name: Josemaria Macedo Carrillo
+# Title: Data pipeline
+# Created: 01/11/24
+# Last modified: 01/17/24
+# DSI
+
+import sys
+
+
+
+# if __name__ == "__main__":
+
+#     if len(sys.argv) == 1:
+#         # Plot data in jupyter notebook with specified country (does the same
+#         # as sending 'plot' as second argument)
+
+#     elif len(sys.argv) == 2:
+#         if sys.argv[1] == "import_data":
+#             # Function to import data
+#         elif sys.argv[1] == "clean_data":
+#             # Function to clean data
+#         elif sys.argv[1] == "transform_data":
+#             # Function to transform data
+#         elif sys.argv[1] == "plot":
+#             # Plot data in jupyter notebook
+#         elif sys.argv[1] == "run_all":
+#             # Function to run the whole data pipeline for that country 
+#         else:
+#             print("Incorrect arguments. Send 'import_data', 'clean_data',\
+#                   'transform_data', 'plot' or 'run_all'.")
