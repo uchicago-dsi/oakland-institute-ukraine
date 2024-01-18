f73ead7200a5c9e9412bb450db4c0f9929e548ee
Josemaria Macedo <josemaria@uchicago.edu>
2024-01-17 19:19:44 -0600

    Added functions from the 'exports_shares.ipynb' notebook

--- utils/clean_data.py
+++ utils/clean_data.py
@@ -1,7 +1,7 @@
 # Name: Josemaria Macedo Carrillo
 # Title: Clean data
 # Created: 07/14/23
-# Last modified: 07/20/23
+# Last modified: 01/17/24
 # DSI
 
 import os
@@ -9,6 +9,13 @@ import sys
 import unicodedata
 from deep_translator import GoogleTranslator, DeeplTranslator
 import pandas as pd
+from .get_data import get_data, compile_data
+from ..config import IG_FILES_PATH, COUNTRY_FILES
+import json
+from .map import top_parent, top_subsidiaries
+import re
+import copy
+from .transform_data import standard_company_name
 
 PRODUCTS_VAL = ["corn", "soya", "sunflower", "wheat", "sunflower", "barley",
                 "peas", "rapeseed", "sunflower", "vegetable", "soya", "canola",
@@ -55,6 +62,15 @@ SUBSIDIARY_DICT = {"enselcoagro": "Kernel Holding", "mhp": "MHP",
                    "druzhbanova": "Kernel Holding",
                    "astarta": "Astarta Holding",
                    "agroprogress": "Industrial Milk \nCompany (IMC)"}
+
+# Import countries dictionaries from JSON file
+f = open('../names.json')
+data = json.load(f)
+
+ASIA_NAME_DICT = data["ASIA_NAME_DICT"]
+SPAIN_NAME_DICT = data["SPAIN_NAME_DICT"]
+BELGIUM_NAME_DICT = data["BELGIUM_NAME_DICT"]
+KNOWN_COMPANIES = data["KNOWN_COMPANIES"]
 
 def keep_chr(ch):
     """
@@ -228,3 +244,285 @@ def clean_column(df, column):
     Return: None. It does the change in place in the input dataframe.
     """
     df[column] = df[column].str.lower()
+
+def clean_data(source, path=None):
+    """
+    Get clean data from data source directory
+    Inputs:
+        source (str): data source, either "ig" (Import Genius), "bsgi" (Black
+            Sea Grain Initiative) or "panjiva".
+
+    Return (DataFrame): dataframe with cleaned data (with renamed columns and
+        new columns neccesary for analysis).
+    """
+    data_sources = ["ig", "bsgi", "panjiva"]
+    assert source in data_sources, "Wrong data source Error: source must be\
+                                    'ig', 'bsgi' or 'panjiva'."
+    
+    if path is None:
+        df = compile_data(source)
+    else:
+        df = get_data(path, source)
+        if source == "ig":
+            df["company_searched"] = df["SHIPPER"]
+    df = rename_columns(df, source)
+    create_columns(df, source)
+    
+    columns = ["country", "product"]
+    for col in columns:
+        clean_column(df, col)
+
+    if source == "bsgi":
+        translate_column(df, "product_std", "google", "en", "uk")
+        translate_column(df, "country", "google", "en", "uk")
+        df = df.rename(columns={"country": "country_en",
+                                "country_gt": "country"})
+        clean_column(df, "country")
+
+    elif source == "panjiva":
+        clean_column(df, "shipment_origin")
+        df = df.loc[df.loc[:, "shipment_origin"] == "ukraine"]
+
+    return df
+
+def generate_path(countries):
+    """
+    Generate path where file is located.
+
+    Inputs:
+       countries (str): countries which we want the path for. It can be either
+            'asia' or 'spain'.
+    
+    Return (str): file path where file is located.
+    """
+    try:
+        country_file = COUNTRY_FILES[countries]
+    except KeyError:
+        print("Wrong countries' name. Use 'asia', 'spain' or 'belgium'.")
+    except:
+        print("Unknown error")
+    
+    path = os.path.join(IG_FILES_PATH, COUNTRY_FILES[countries])
+
+    return path
+
+def correct_name(series, name_dict):
+    """
+    Update column values from a dataframe with correct ones.
+
+    Inputs:
+        series (Series): column we want to update
+        name_dict (dict): dictionary with incorrect names as keys and correct
+            names as values.
+    
+    Returns: series with correct names.
+    """
+    return series.apply(lambda x: name_dict[x])
+
+def filter_country(deals, country):
+    """
+    Filter land deals for a specific country
+
+    Inputs:
+        deals (DataFrame): dataframe with land deals data
+        country (str): country name
+
+    Returns (DataFrame): dataframe with country land locations
+    """
+    deals_c = deals.copy()
+    deals_c.loc[:, "country_low"] = deals_c.loc[:, "Target country"].str.lower()
+    deals_c = deals_c.loc[deals_c.loc[:, "country_low"].str.contains(country.lower(),
+                                                                     na=False)]
+    
+    return deals_c
+
+def extend_list(list, character):
+    """
+    Create new list with split text based on "character" parameter
+
+    Inputs:
+        list (lst): list of strings
+        character (str): character to use to separate string values in list
+
+    Returns (lst): new list with split text based on "character" parameter
+    """
+    new_lst = []
+    for value in list:
+        new_lst += value.split(character)
+
+    return new_lst
+
+def clean_list(list, pattern):
+    """
+    Create new list with clean text based on "pattern" parameter
+
+    Inputs:
+        list (lst): list of strings
+        pattern (str): pattern we want to find and replace in each string value
+            on the list
+
+    Returns (lst): None. Makes changes in place in input "list".
+    """
+    for i, value in enumerate(list):
+        match = re.search(pattern, value)
+        if match:
+            list[i] = match.group(0)
+
+def parent_subsidiary_dict(parent_lst, deals_data, n_subsidiaries):
+    """
+    Create dictionary with parent companies as keys and subsidaries as values.
+
+    Inputs:
+        parent_lst (lst): list of parent companies
+        deals_data (DataFrame): deals dataframe where we're going to look for
+            subsidiaries
+        n_subsidiaries (int): number of subsidiaries we want to get for parent
+            company.
+
+    Returns (dict): dictionary with parent companies as keys and subsidaries as
+        values.
+    """
+    subsidiaries_dict = {}
+
+    for parent in parent_lst:
+        subsidiaries_dict[parent] = top_subsidiaries(deals_data, parent,
+                                                     n_subsidiaries)
+
+    return subsidiaries_dict
+
+def translate_subsidiaries(dict, source_lan, target_lan):
+    """
+    Create dictionary with translated subsidiaries' names as values.
+
+    Inputs:
+        dict (dict): dictionary with subsidarie names in English
+        source_lan (str): current language code of values of dictionary
+        target_lan (str): target language code we want to translate values to
+
+
+    Returns (dict): dictionary with parent companies as keys and subsidaries as
+        values.
+    """
+    d_target = {}
+
+    for parent, sub_lst in dict.items():
+        target_lst = []
+        for subsidiary in sub_lst:
+            target_lst.append(GoogleTranslator(source_lan, target_lan).\
+                              translate(subsidiary))
+        d_target[parent] = target_lst
+
+    return d_target
+
+def dict_to_lower(dict):
+    """
+    Convert dictionary values of 'dict' to lowercase
+
+    Inputs:
+        dict (dict): dictionary with parent companies as keys and subsidaries
+            as values.
+
+    Returns (dict): None. The function does the change in place of the input
+        'dict'.
+    """
+    for parent, subsidiary_lst in dict.items():
+        for i, subsidiary in enumerate(subsidiary_lst):
+            subsidiary_lst[i] = subsidiary.lower()
+
+def add_companies_manually(old_dict, new_dict):
+    """
+    Add non-repeated values from 'new_dict' to 'old_dict' based on keys.
+
+    Inputs:
+        old_dict (dict): dictionary with parent companies as keys and
+            subsidaries as values.
+        new_dict (dict): dictionary with parent companies as keys and new
+            subsidaries as values.
+
+    Returns (dict): dictionary with non-repeated values from 'new_dict' to
+        'old_dict' based on keys.
+    """
+    old_dict_copy = copy.deepcopy(old_dict)
+    
+    for parent, subsidiary_lst in new_dict.items():
+        for i, subsidiary in enumerate(subsidiary_lst):
+            subsidiary_lst[i] = subsidiary.lower()
+
+        if parent in old_dict_copy:
+            # old_dict_copy[parent] += new_dict[parent]
+            old_dict_copy[parent].extend(subsidiary_lst)
+        else:
+            # old_dict_copy[parent] = new_dict[parent]
+            old_dict_copy[parent] = subsidiary_lst
+    
+    return old_dict_copy
+
+def create_subsidiary_dict(n_parent_companies, n_subsidiaries, ig_data):
+    """
+    Create dictionary with parent companies as keys and subsidiaries as values
+        using Land Matrix data.
+
+    n_parent_companies (int): top 'n' parent companies we want to 
+    """
+    # Import Land Matrix data and create list with top parent companies
+    deals = pd.read_csv("../data/land_matrix/deals.csv", delimiter=";")
+    deals_c = filter_country(deals, "ukraine")
+    parent_lst = top_parent(deals_c, n_parent_companies)
+    parent_lst = extend_list(parent_lst, "|")
+
+    # Regex cleans parent company names. Names include deal_id as 'Kernel #366#'
+    # Function removes specified regex pattern.
+    pattern = r'^.*?(?=#\d+#)'
+    clean_list(parent_lst, pattern)
+
+    # Create dictionary with parent companies as keys and subsidiaries as values
+    subsidiaries_dict = parent_subsidiary_dict(parent_lst, deals_c, n_subsidiaries)
+
+    # Translate subsidiary names to Ukrainian to try to match them to IG shipper
+    # names which are in Ukrainian
+    subsidiaries_uk = translate_subsidiaries(subsidiaries_dict, "en", "uk")
+
+    # Turn company values to lowercase both in subsidiary dictionary and IG
+    # shipper column
+    dict_to_lower(subsidiaries_uk)
+    ig_data["shipper_low"] = ig_data["shipper"].str.lower()
+
+    # Manually add some companies we identified separately
+    subsidiaries_c = add_companies_manually(subsidiaries_uk, KNOWN_COMPANIES)
+
+    return subsidiaries_c
+
+def clean_bsgi_by_country(countries):
+    """
+    Get Black Sea Grain Initiative (BSGI) data for specific countries.
+
+    Inputs:
+        countries (str): countries which we want the path for. It can be either
+            'asia' or 'spain'.
+
+    Returns (DataFrame): table with filtered BSGI data.
+    """
+    
+    # assert_countries(countries)
+    bsgi = clean_data("bsgi")
+
+    # We filter only corresponding months and countries in BSGI dataset
+    
+    if countries == "asia":
+        bsgi_country = bsgi[(bsgi["date"] >= "2022-08-01") &
+                            (bsgi["date"] < "2023-04-01") &
+                            ((bsgi["country_en"] == "india") |
+                             (bsgi["country_en"] == "sri lanka") |
+                             (bsgi["country_en"] == "viet nam"))]
+    elif countries == "spain":
+        bsgi_country = bsgi[(bsgi["date"] >= "2022-08-01") &
+                            (bsgi["date"] < "2023-04-01") &
+                            (bsgi["country_en"] == "spain")]
+    elif countries == "belgium":
+        bsgi_country = bsgi[(bsgi["date"] >= "2022-08-01") &
+                            (bsgi["date"] < "2023-04-01") &
+                            (bsgi["country_en"] == "belgium")]
+    else:
+        print("Wrong countries' name. Use 'asia', 'spain' or 'belgium'.")
+
+    return bsgi_country
