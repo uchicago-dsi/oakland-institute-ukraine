17d82fb66fdcec3e2302a655ea7a3393bfe44099
Josemaria Macedo <josemaria@uchicago.edu>
2024-01-18 17:04:00 -0600

    Added 'CLEAN_FILES' dictionary to store paths for clean files

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ ROOT_PATH = os.getcwd()
 IG_FILES_PATH = os.path.join(ROOT_PATH, "data/ig")
 COUNTRY_FILES = {"asia": "ig_asia_22-23.csv", "spain": "ig_spain_22-23.csv",
                  "belgium": "ig_belgium_22-23.csv"}
+CLEAN_FILES = {"asia": "ig_clean_asia.csv", "spain": "ig_clean_spain.csv",
+                 "belgium": "ig_clean_belgium.csv"}
