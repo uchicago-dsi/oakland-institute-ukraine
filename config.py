37bd6556a61df4236450304f4a6b48b0bf7c3bdf
Josemaria Macedo <josemaria@uchicago.edu>
2024-01-25 14:18:43 -0600

    Had to change ROOT_PATH to be able to run the pipeline.py file without errors

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 import os
 
-ROOT_PATH = os.path.dirname(os.getcwd())
-# Path that runs correctly when we run python pipeline.py from terminal
-# ROOT_PATH = os.getcwd()
+# ROOT_PATH = os.path.dirname(os.getcwd()) # Path that runs correctly when we run Jupyter notebook
+ROOT_PATH = os.getcwd() # path that runs correctly when running python pipeline.py
 IG_FILES_PATH = os.path.join(ROOT_PATH, "data/ig")
 COUNTRY_FILES = {"asia": "ig_asia_22-23.csv", "spain": "ig_spain_22-23.csv",
                  "belgium": "ig_belgium_22-23.csv"}
